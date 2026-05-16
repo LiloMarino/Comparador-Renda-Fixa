6ab3033554ce264bf676672e6fb0742053709d6c
LiloMarino <lilo.marino@hotmail.com>
2026-05-16 13:03:57 -0300

    build: Configurado deploy no pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Comparador-Renda-Fixa/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
