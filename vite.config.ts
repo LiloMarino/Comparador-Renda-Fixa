e930c5a20897f3964aa1032e0ebabda4333b2f11
LiloMarino <lilo.marino@hotmail.com>
2026-05-16 15:16:36 -0300

    build: Adicionado shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
@@ -13,6 +14,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss()
   ],
 })
